e1ecc8155cd384609c6711881229ea32f1f4cada
Peter Berck <peter@berck.se>
2012-01-26 15:01:50 +0100

    Web PDT files added.

---
+++ etc/pdt/wopr.php
@@ -0,0 +1,39 @@
+<?php
+error_reporting(E_ERROR);
+
+$wopr_port = "1984";
+$wopr_host = "localhost";
+
+if ( isset( $_POST['p'] ) ) {
+   $wopr_port = $_POST['p'];
+}
+
+$address = gethostbyname($wopr_host);
+if ( isset( $_POST['h'] ) ) {
+   $address = $_POST['h'];
+}
+
+$cmd = "CTX";
+if ( isset( $_POST['c'] ) ) {
+   $cmd = $_POST['c'];
+}
+
+$socket = fsockopen($address, $wopr_port, $errno, $errstr, 30);
+
+if ( $socket ) {
+  fwrite( $socket, $cmd."\n" );
+
+  stream_set_timeout($socket, 5);
+
+  if ( ! feof($socket) ) { // if was while.
+    echo fgets( $socket );
+  }
+  $info = stream_get_meta_data($socket);
+  if ($info['timed_out']) {
+    echo "<result>time out</result>";
+  }
+  fclose( $socket );
+ } else {
+  echo "<result>no socket</result>";
+}
+?> 
